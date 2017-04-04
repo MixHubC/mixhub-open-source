6ab73f126c38503d19a7f61d4c32157b02cf6910
Your Name <you@example.com>
2017-04-04 03:06:16 -0700

    test4

--- src/test/java/online/themixhub/MiscUtilsTest.java
+++ src/test/java/online/themixhub/MiscUtilsTest.java
@@ -1,36 +1,33 @@
 package online.themixhub;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
 
-import online.themixhub.MiscUtils;
+import online.themixhub.demo.utils.MiscUtils;
+import org.junit.Test;
 
 public class MiscUtilsTest {
 
 
-   @Test
-   public void exceptionToStringIsNotNull() {
-        String result = null;
-        Exception e = new Exception("Test Exception");
-        result = MiscUtils.exceptionToString(e); 
-        assertNotNull(result); 
-  }
+	@Test
+	public void exceptionToStringIsNotNull() {
+		String result = null;
+		Exception e = new Exception("Test Exception");
+		result = MiscUtils.exceptionToString(e);
+		assertNotNull(result);
+	}
 
 
-   @Test
-   public void stringSizeLengthFileIsNotNull() {
-        String result = null;
-        long l = 1L;
-        result = MiscUtils.getStringSizeLengthFile(l); 
-        assertNotNull(result);
-        System.out.println(result); 
-  }
+	@Test
+	public void stringSizeLengthFileIsNotNull() {
+		String result = null;
+		long l = 1L;
+		result = MiscUtils.getStringSizeLengthFile(l);
+		assertNotNull(result);
+		System.out.println(result);
+	}
 
 
 
 }
-
 
 
