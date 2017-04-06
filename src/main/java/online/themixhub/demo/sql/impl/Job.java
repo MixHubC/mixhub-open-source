041ab32cc9e221c496fa69b0803da136e808a9dd
Scott Hommel <scott@themixhub.online>
2017-04-05 20:15:23 -0400

    Added some basic encapsulation

--- src/main/java/online/themixhub/demo/sql/impl/Job.java
+++ src/main/java/online/themixhub/demo/sql/impl/Job.java
@@ -12,14 +12,14 @@ import java.util.List;
  */
 public class Job {
 
-    int id;
-    int owner_id;
-	int engineer_id;
-	int stage;
-    String title;
-    long date;
-    int anonymous_engineer;
-    long last_activity_date;
+    private int id;
+    private int owner_id;
+    private int engineer_id;
+    private int stage;
+    private String title;
+    private long date;
+    private int anonymous_engineer;
+    private long last_activity_date;
 
     public int getId() {
         return id;
@@ -29,59 +29,59 @@ public class Job {
         this.id = id;
     }
 
-	public int getOwner_id() {
-		return owner_id;
-	}
+    public int getOwner_id() {
+	return owner_id;
+    }
 
-	public void setOwner_id(int owner_id) {
-		this.owner_id = owner_id;
-	}
+    public void setOwner_id(int owner_id) {
+	this.owner_id = owner_id;
+    }
 
-	public int getEngineer_id() {
-		return engineer_id;
-	}
+    public int getEngineer_id() {
+	return engineer_id;
+    }
 
-	public void setEngineer_id(int engineer_id) {
-		this.engineer_id = engineer_id;
-	}
+    public void setEngineer_id(int engineer_id) {
+	this.engineer_id = engineer_id;
+    }
 
-	public int getStage() {
-		return stage;
-	}
+    public int getStage() {
+	return stage;
+    }
 
-	public void setStage(int stage) {
-		this.stage = stage;
-	}
+    public void setStage(int stage) {
+	this.stage = stage;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+	return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+	this.title = title;
+    }
 
-	public long getDate() {
-		return date;
-	}
+    public long getDate() {
+	return date;
+    }
 
-	public void setDate(long date) {
-		this.date = date;
-	}
+    public void setDate(long date) {
+	this.date = date;
+    }
 
-	public int getAnonymous_engineer() {
-		return anonymous_engineer;
-	}
+    public int getAnonymous_engineer() {
+	return anonymous_engineer;
+    }
 
-	public void setAnonymous_engineer(int anonymous_engineer) {
-		this.anonymous_engineer = anonymous_engineer;
-	}
+    public void setAnonymous_engineer(int anonymous_engineer) {
+	this.anonymous_engineer = anonymous_engineer;
+    }
 
-	public long getLast_activity_date() {
-		return last_activity_date;
-	}
+    public long getLast_activity_date() {
+	return last_activity_date;
+    }
 
-	public void setLast_activity_date(long last_activity_date) {
-		this.last_activity_date = last_activity_date;
-	}
+    public void setLast_activity_date(long last_activity_date) {
+	this.last_activity_date = last_activity_date;
+    }
 }
